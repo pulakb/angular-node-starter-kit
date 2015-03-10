14838c1c6f8ece08565ebb4c4bbcc35e0d847376
pulakb <bhattacharyyapulak@gmail.com>
2015-03-10 15:56:30 +0530

    states added

--- client/gulpFile.js
+++ client/gulpFile.js
@@ -23,53 +23,75 @@ var buildConfig = require( './build.config.js' );
 // Tasks
 // =====
 
+var cssStream,
+	opts = {
+	    conditionals: true,
+	    spare:true
+	  };
+
 /** CSS
 * 1. Strip comments
 * 2. Minify files
 * 3. Concat files as 'styles.css'
 */
-var cssStream;
-
 gulp.task('minify-css', function() {
   cssStream = gulp.src(buildConfig.app_files.css)
-  	.pipe(stripCssComments())
-    .pipe(minifyCSS())
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('./dist/css'));
+	  	.pipe(stripCssComments())
+	    .pipe(minifyCSS())
+	    .pipe(concat('styles.css'))
+	    .pipe(gulp.dest('./dist/css'));
 });
 
 // Lint Task
 gulp.task('lint', function() {
   return gulp.src(buildConfig.app_files.js)
-    .pipe(jshint())
-    .pipe(jshint.reporter('gulp-jshint-html-reporter', {
-      filename: buildConfig.debug_dir + '/jshint-output.html'
-    }));
+    	.pipe(jshint())
+	    .pipe(jshint.reporter('gulp-jshint-html-reporter', {
+	      filename: buildConfig.debug_dir + '/jshint-output.html'
+	    }));
 });
 
+// Script Task - Application & Vendor
 gulp.task('scripts', function () {
 	// Concatenate vendor scripts 
 	var vendorStream = gulp.src(buildConfig.vendor_files.js)
-	  .pipe(concat('vendors.js'))
-	  .pipe(gulp.dest('./dist/js'));
+	  	.pipe(concat('vendors.js'))
+	  	.pipe(gulp.dest('./dist/js'));
 
 
 	// Concatenate AND minify app sources 
 	var appStream = gulp.src(buildConfig.app_files.js)
-	  .pipe(concat('application.js'))
-	  .pipe(uglify())
-	  .pipe(gulp.dest('./dist/js'));
+		.pipe(stripDebug())
+	  	.pipe(concat('application.js'))
+	  	.pipe(uglify())
+	  	.pipe(gulp.dest('./dist/js'));
 
   return gulp.src('./src/index.html')
-  .pipe(inject(series(cssStream)))
-  .pipe(inject(series(vendorStream, appStream)))
-  .pipe(gulp.dest('./dist'));
+  	.pipe(inject(series(cssStream)))
+  	.pipe(inject(series(vendorStream, appStream)))
+  	.pipe(gulp.dest('./dist'));
 
 });
 
+// Minify HTML
+gulp.task('minify-html', function() {
+  var opts = {
+    conditionals: true,
+    spare:true,
+    comments: false
+  };
+ 
+  return gulp.src('./src/index.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./dist'));
+});
 
-
+// Watch Files For Changes - Not working 
+/*gulp.task('watch', function() {
+    gulp.watch(buildConfig.app_files.js, ['lint', 'scripts']);
+    gulp.watch(buildConfig.app_files.css, ['minify-css']);
+});*/
 
 // Default Task
-gulp.task('default', ['minify-css', 'lint', 'scripts']);
+gulp.task('default', [ 'minify-css', 'lint', 'scripts','minify-html'/*, 'watch' */]);
 
--- client/src/app/module-1/module-1.js
+++ client/src/app/module-1/module-1.js
@@ -1,3 +1,6 @@
 /**
 * All module-1 code
 */
+var p = 19;
+console.log('module-1 js file');
+var q = 29;
